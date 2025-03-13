e376940b84d69cd56c173fed2cc974ce8827be8e
Nguyễn Trung Nguyên <31590813+NguyenKeo@users.noreply.github.com>
2025-03-13 18:16:48 +0700

    Task 06: Popular Collections

---
+++ task06/script.js
@@ -0,0 +1,96 @@
+const collectionList = [
+	{
+		id: 1,
+		avata: 'image 31.jpg',
+		title: 'AHRI',
+		alt: 'Lelah Nichols'
+	},
+	{
+		id: 2,
+		avata: 'image 32.png',
+		title: 'AKALI',
+		alt: 'Jesus Weiss'
+	},
+	{
+		id: 3,
+		avata: 'image 33.png',
+		title: 'AZIR',
+		alt: 'Annie Rice'
+	},
+	{
+		id: 4,
+		avata: 'image 34.png',
+		title: 'CAITLYN',
+		alt: 'Robert Brower'
+	},
+	{
+		id: 5,
+		avata: 'image 35.png',
+		title: 'DARIUS',
+		alt: 'Amy Campbell'
+	},
+	{
+		id: 6,
+		avata: 'image 36.png',
+		title: 'FIZZ',
+		alt: 'Anthony S. Morin'
+	},
+	{
+		id: 7,
+		avata: 'image 37.png',
+		title: 'IRELIA',
+		alt: 'Anthony S. Morin'
+	},
+	{
+		id: 8,
+		avata: 'image 38.png',
+		title: 'JINX',
+		alt: 'Anthony S. Morin'
+	},
+	{
+		id: 9,
+		avata: 'image 39.png',
+		title: 'RENGAR',
+		alt: 'Anthony S. Morin'
+	},
+	{
+		id: 10,
+		avata: 'image 40.png',
+		title: 'SERAPHINE',
+		alt: 'Anthony S. Morin'
+	},
+	{
+		id: 11,
+		avata: 'image 41.png',
+		title: 'YASUO',
+		alt: 'Anthony S. Morin'
+	},
+	{
+		id: 12,
+		avata: 'image 42.png',
+		title: 'WUKONG',
+		alt: 'Anthony S. Morin'
+	}
+];
+// const collectionListElement = document.getElementById('collections-list');
+// collectionListElement.innerHTML = Array.isArray(collectionList)
+// 	? collectionList
+// 			.map(
+// 				item => `
+//
+// 				<div class="collections-item">
+//                     <div class="collections-item-wrap">
+//                         <img src="./images/1.png" alt="collection" class="w-full">
+//
+//                         <div class="collections-item__imgs-sub">
+//                             <img src="./images/1.1.png" alt="collection">
+//                             <img src="./images/1.2.png" alt="collection">
+//                             <img src="./images/1.3.png" alt="collection">
+//                         </div>
+//                     </div>
+//                 </div>
+//
+//         	`
+// 			)
+// 			.join('')
+// 	: 'No products!';

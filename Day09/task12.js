ff2754d70ad692fa92fc1ff98bfd46629ff36dc2
Nguyễn Trung Nguyên <31590813+NguyenKeo@users.noreply.github.com>
2025-03-20 11:57:18 +0700

    Task 121 bổ xung đầu vào nhập liệu

--- Day09/task12.js
+++ Day09/task12.js
@@ -1,21 +1,30 @@
+const num = parseFloat(
+	prompt(
+		'Task 12: Nhập số vào giới hạn để kiếm tra trong khoảng có bao số chính phương: '
+	)
+);
+
 // Input:
-function printSquareNumber(n) {
+function printSquareNumber() {
 	// Xử lý và in ra kết quả
-	if (n) {
+	console.log('num: ', num, typeof num);
+	if (num && typeof num === 'number' && !isNaN(num)) {
+		console.log('Đầu vào là dạng số hợp lệ')
 		let result = [];
-		for (let i = 2; i <= n; i++) {
+		for (let i = 2; i <= num; i++) {
 			if (Number.isInteger(Math.sqrt(i))) {
 				result.push(i);
 			}
 		}
 		console.log(result);
+		alert(`Trong khoảng từ 1 đến ${num} có ${result.length} số chính phương là các số: ${result.join(', ')}`);
 	}
 }
 
 // Output:
-printSquareNumber(10);
+printSquareNumber();
 console.log('--------------------');
-printSquareNumber(20);
+// printSquareNumber(20);
 
 // console.log(printSquareNumber(10)); // 4 9
 // console.log(printSquareNumber(20));  // 4 9 16
